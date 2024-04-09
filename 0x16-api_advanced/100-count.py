4dd03e29c861a756b191579a7fff9361d9db62ec
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-09 14:15:01 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,13 +1,34 @@
-#!/usr/bin/python3
-"""
-100-main
-"""
-import sys
+import requests
 
-if __name__ == '__main__':
-    count_words = __import__('100-count').count_words
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+def count_words(subreddit, word_list, after=None, counts=None):
+    if counts is None:
+        counts = {}
+
+    if after is None:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     else:
-        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
+        
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return
+
+    data = response.json()
+
+    for post in data['data']['children']:
+        title = post['data']['title'].lower()
+        for word in word_list:
+            word = word.lower()
+            if ' ' + word + ' ' in title or title.startswith(word + ' ') or title.endswith(' ' + word) or title == word:
+                counts[word] = counts.get(word, 0) + 1
+
+    next_after = data['data']['after']
+    if next_after:
+        return count_words(subreddit, word_list, next_after, counts)
+    else:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print("{}: {}".format(word, count))
