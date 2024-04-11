5499ccdcf221d713af03c8191f785e152efd412f
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-11 23:51:43 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Reddit Sub Counter."""
+""" Exporting csv files"""
+import json
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Reddit Sub Counter."""
-    import requests
-
-    the_results = requests.get("https://www.reddit.com/r/{}/about.json"
-                               .format(subreddit),
-                               headers={"User-Agent": "Erick_N"},
-                               allow_redirects=False)
-    if the_results.status_code >= 300:
-        return 0
-
-    return the_results.json().get("data").get("subscribers")
-    
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
+    else:
+        return(0)
