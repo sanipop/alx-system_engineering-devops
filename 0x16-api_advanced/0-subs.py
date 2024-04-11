ce28f6eaf1a0d39876afac88a8fa8fd19b74dedf
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-12 00:51:55 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,12 +13,14 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
 
-    if req.status_code == 200:
+    if results.status_code == 200:
         return req.json().get("data").get("subscribers")
     else:
         return 0
