ea707e41a71fe4b071e266f1f738dbbbea1416e1
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-11 23:29:24 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
-import requests
+"""Reddit Sub Counter."""
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """Reddit Sub Counter."""
+    import requests
+
+    the_results = requests.get("https://www.reddit.com/r/{}/about.json"
+                               .format(subreddit),
+                               headers={"User-Agent": "Erick_N"},
+                               allow_redirects=False)
+    if the_results.status_code >= 300:
         return 0
+
+    return the_results.json().get("data").get("subscribers")
+    
