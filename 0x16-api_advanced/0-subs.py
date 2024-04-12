7f70b5b8a0cd4d29800179739deb859179f8768d
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-12 01:27:47 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     if not subreddit or type(subreddit) is not str:
         return 0
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
+    headers = {'User-Agent': 'sanipop_one_:v1.0.0'}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
         r = r.json()
