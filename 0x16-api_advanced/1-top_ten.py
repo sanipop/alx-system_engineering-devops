0a651b6a9b603a88b5706d25c41ec58f55203b0c
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-12 01:25:01 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""This is the module to handle requests for sending
+HTTP requests to the Reddit API"""
 import requests
 
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/sanipop_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+def number_of_subscribers(subreddit):
+    """This is function that queries the Reddit API
+    and returns the number of subscribers"""
+    if not subreddit or type(subreddit) is not str:
+        return 0
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        r = r.json()
+    else:
+        return 0
+    return r.get('data', {}).get('subscribers', 0)
