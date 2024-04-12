0838ae9adae2830fcb302bbc26cd8a4ccab98675
sanipop <67932273+sanipop@users.noreply.github.com>
2024-04-12 01:32:24 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
-If the subreddit is invalid, it prints None.
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'bnon-t79'}  # Set a custom User-Agent to avoid issues
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Litert_me'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
 
-if __name__ == '__main__':
-    import sys
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        top_ten(subreddit)
+if __name__ == "__main__":
+    top_ten(argv[1])
